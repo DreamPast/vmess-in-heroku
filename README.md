628d0db1d937e140edda2372efd076c5b3a321fc
DreamPast <40065902+DreamPast@users.noreply.github.com>
2019-02-12 16:58:30 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+# V2ray Fast Deploy
+
 [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+---
+
+### Description
+
+VER : version
+
+V2_CONF: config.json
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,58 @@
-#cd /v2raybin
-#echo -e "$CONFIG_JSON" > config.json
-#if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-#  echo -e "$CERT_PEM" > cert.pem
-#  echo -e "$KEY_PEM"  > key.pem
-#fi
+#! /bin/bash
+rm -rf /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+date -R
 
-cd /v2ray
-#echo -e "$CONFIG_JSON" > config.json
-#ls -l
-wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
-mkdir v2ray-v$VER-linux-64
-unzip v2ray.zip -d v2ray-v$VER-linux-64
-#ls -lR
-mv v2ray-v$VER-linux-64/v2ray .
-mv v2ray-v$VER-linux-64/v2ctl .
-mv v2ray-v$VER-linux-64/geoip.dat .
-mv v2ray-v$VER-linux-64/geosite.dat .
-rm -fR /v2ray/v2ray-v$VER-linux-64
-#unzip -h
+SYS_Bit="$(getconf LONG_BIT)"
+[[ "$SYS_Bit" == '32' ]] && BitVer='_linux_386.tar.gz'
+[[ "$SYS_Bit" == '64' ]] && BitVer='_linux_amd64.tar.gz'
 
-chmod +x v2ray v2ctl
-sed -i "s/your_uuid/$UUID/g" config.json
-sed -i "s/your_port/$PORT/g" config.json
-./v2ray
+if [ "$VER" = "latest" ]; then
+  V_VER=`wget -qO- "https://api.github.com/repos/v2ray/v2ray-core/releases/latest" | grep 'tag_name' | cut -d\" -f4`
+else
+  V_VER="v$VER"
+fi
+
+mkdir /v2raybin
+cd /v2raybin
+wget --no-check-certificate -qO 'v2ray.zip' "https://github.com/v2ray/v2ray-core/releases/download/$V_VER/v2ray-linux-$SYS_Bit.zip"
+unzip v2ray.zip
+rm -rf v2ray.zip
+chmod +x /v2raybin/v2ray-$V_VER-linux-$SYS_Bit/*
+
+C_VER=`wget -qO- "https://api.github.com/repos/mholt/caddy/releases/latest" | grep 'tag_name' | cut -d\" -f4`
+mkdir /caddybin
+cd /caddybin
+wget --no-check-certificate -qO 'caddy.tar.gz' "https://github.com/mholt/caddy/releases/download/$C_VER/caddy_$C_VER$BitVer"
+tar xvf caddy.tar.gz
+rm -rf caddy.tar.gz
+chmod +x caddy
+cd /root
+mkdir /wwwroot
+cd /wwwroot
+
+wget --no-check-certificate -qO 'demo.tar.gz' "https://github.com/DreamPast/vmess-in-heroku/raw/master/demo.tar.gz"
+tar xvf demo.tar.gz
+rm -rf demo.tar.gz
+
+cat <<-EOF > /v2raybin/v2ray-$V_VER-linux-$SYS_Bit/config.json
+${V2_CONF}
+EOF
+
+cat <<-EOF > /caddybin/Caddyfile
+http://0.0.0.0:${PORT}
+{
+	root /wwwroot
+	index index.html
+	timeouts none
+	proxy /FreeApp localhost:2333 {
+		websocket
+		header_upstream -Origin
+	}
+}
+EOF
+
+cd /v2raybin/v2ray-$V_VER-linux-$SYS_Bit
+./v2ray &
+cd /caddybin
+./caddy -conf="Caddyfile"
