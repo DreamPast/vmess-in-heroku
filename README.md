3b620464bc9ae654e9a0833f78e8038038e8beff
DreamPast <40065902+DreamPast@users.noreply.github.com>
2019-02-12 17:58:36 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # V2ray Fast Deploy
 
 [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
-
----
-
-### Description
-
-VER : version
-
-V2_CONF: config.json
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,20 @@
 #! /bin/bash
+if [[ -z "${UUID}" ]]; then
+  UUID="4890bd47-5180-4b1c-9a5d-3ef686543112"
+fi
+
+if [[ -z "${AlterID}" ]]; then
+  AlterID="10"
+fi
+
+if [[ -z "${V2_Path}" ]]; then
+  V2_Path="/FreeApp"
+fi
+
+if [[ -z "${V2_QR_Path}" ]]; then
+  V2_QR_Code="1234"
+fi
+
 rm -rf /etc/localtime
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 date -R
@@ -36,7 +52,36 @@ tar xvf demo.tar.gz
 rm -rf demo.tar.gz
 
 cat <<-EOF > /v2raybin/v2ray-$V_VER-linux-$SYS_Bit/config.json
-${V2_CONF}
+{
+    "log":{
+        "loglevel":"warning"
+    },
+    "inbound":{
+		"protocol":"vmess",
+        "listen":"127.0.0.1",
+        "port":2333,
+        "settings":{
+            "clients":[
+                {
+                    "id":"${UUID}",
+					"security": "auto",
+                    "alterId":${AlterID}
+                }
+            ]
+        },
+        "streamSettings":{
+            "network":"ws",
+            "wsSettings":{
+                "path":"${V2_Path}"
+            }
+        }
+    },
+    "outbound":{
+        "protocol":"freedom",
+        "settings":{
+        }
+    }
+}
 EOF
 
 cat <<-EOF > /caddybin/Caddyfile
@@ -45,12 +90,38 @@ http://0.0.0.0:${PORT}
 	root /wwwroot
 	index index.html
 	timeouts none
-	proxy /FreeApp localhost:2333 {
+	proxy ${V2_Path} localhost:2333 {
 		websocket
 		header_upstream -Origin
 	}
 }
 EOF
+
+cat <<-EOF > /v2raybin/vmess.json 
+{
+    "v": "2",
+    "ps": "${AppName}.herokuapp.com",
+    "add": "${AppName}.herokuapp.com",
+    "port": "443",
+    "id": "${UUID}",
+    "aid": "${AlterID}",			
+    "net": "ws",			
+    "type": "none",			
+    "host": "",			
+    "path": "${V2_Path}",	
+    "tls": "tls"			
+}
+EOF
+
+if [ "$AppName" = "no" ]; then
+  echo "不生成二维码"
+else
+  mkdir /wwwroot/$V2_QR_Path
+  vmess="vmess://$(cat /v2raybin/vmess.json | base64 -w 0)" 
+  Linkbase64=$(echo -n "${vmess}" | tr -d '\n' | base64 -w 0) 
+  echo "${Linkbase64}" | tr -d '\n' > /wwwroot/$V2_QR_Path/index.html
+  echo -n "${vmess}" | qrencode -s 6 -o /wwwroot/$V2_QR_Path/v2.png
+fi
 
 cd /v2raybin/v2ray-$V_VER-linux-$SYS_Bit
 ./v2ray &
